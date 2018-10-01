fd389ecbcf73eed9b587d39735841aff5e1eb54c
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-01 16:12:32 +0100

    Add injector class
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

---
+++ app/src/main/java/app/a2ms/dagger2/di/Injector.java
@@ -0,0 +1,13 @@
+package app.a2ms.dagger2.di;
+
+import android.app.Activity;
+
+public class Injector {
+    private Injector() {
+
+    }
+
+    public static void inject(Activity activity) {
+
+    }
+}

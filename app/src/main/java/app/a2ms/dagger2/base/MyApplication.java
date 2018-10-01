2c5254f771461a9b29c58558e5a942aeac9d196c
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-01 15:46:04 +0100

    Build the Application Component

--- app/src/main/java/app/a2ms/dagger2/base/MyApplication.java
+++ app/src/main/java/app/a2ms/dagger2/base/MyApplication.java
@@ -3,8 +3,15 @@ package app.a2ms.dagger2.base;
 import android.app.Application;
 
 public class MyApplication extends Application {
+    //make a field for {@ApplicationComponent}
+    private ApplicationComponent component;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        //always remember to rebuild the project to get Dagger builders
+        component = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
     }
 }

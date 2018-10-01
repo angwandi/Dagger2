fcdaa33774aeedcb8504e374ade737ad66f566bd
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-01 15:30:43 +0100

    Add an Application SubClass

---
+++ app/src/main/java/app/a2ms/dagger2/base/MyApplication.java
@@ -0,0 +1,10 @@
+package app.a2ms.dagger2.base;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

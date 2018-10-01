bcf5e396c344382e35b4a8a4b4a7ab04534693a2
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-01 19:13:50 +0100

    ScreenInjector Part 02
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

--- app/src/main/java/app/a2ms/dagger2/base/BaseActivity.java
+++ app/src/main/java/app/a2ms/dagger2/base/BaseActivity.java
@@ -1,22 +1,34 @@
 package app.a2ms.dagger2.base;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+
+import com.bluelinelabs.conductor.Conductor;
+import com.bluelinelabs.conductor.Controller;
+import com.bluelinelabs.conductor.ControllerChangeHandler;
+import com.bluelinelabs.conductor.Router;
 
 import java.util.UUID;
 
 import javax.inject.Inject;
 
+import app.a2ms.dagger2.R;
 import app.a2ms.dagger2.di.Injector;
 import app.a2ms.dagger2.di.ScreenInjector;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     private static String INSTANCE_ID_KEY = "instance_id";
-    private String instanceId;
     @Inject
     ScreenInjector screenInjector;
+
+    private String instanceId;
+
+    private Router router;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,9 +37,23 @@ public abstract class BaseActivity extends AppCompatActivity {
         } else {
             instanceId = UUID.randomUUID().toString();
         }
+        setContentView(layoutRes());
+
+        ViewGroup screenContainer = findViewById(R.id.scree_container);
+        if (screenContainer == null) {
+            throw new IllegalArgumentException("Activity must have a view with id : screen_container");
+        }
+
         Injector.inject(this);
         super.onCreate(savedInstanceState);
+
+        router = Conductor.attachRouter(this, screenContainer, savedInstanceState);
+        monitorBackStack();
     }
+
+
+    @LayoutRes
+    protected abstract int layoutRes();
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -49,5 +75,31 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     public ScreenInjector getScreenInjector() {
         return screenInjector;
+    }
+
+    private void monitorBackStack() {
+        router.addChangeListener(new ControllerChangeHandler.ControllerChangeListener() {
+            @Override
+            public void onChangeStarted(
+                    @Nullable Controller to,
+                    @Nullable Controller from,
+                    boolean isPush,
+                    @NonNull ViewGroup container,
+                    @NonNull ControllerChangeHandler handler) {
+
+            }
+
+            @Override
+            public void onChangeCompleted(
+                    @Nullable Controller to,
+                    @Nullable Controller from,
+                    boolean isPush,
+                    @NonNull ViewGroup container,
+                    @NonNull ControllerChangeHandler handler) {
+                if (!isPush && from != null) {
+                    Injector.clearComponent(from);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/app/a2ms/dagger2/home/MainActivity.java
@@ -0,0 +1,12 @@
+package app.a2ms.dagger2.home;
+
+import app.a2ms.dagger2.R;
+import app.a2ms.dagger2.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int layoutRes() {
+        return R.layout.activity_main;
+    }
+}

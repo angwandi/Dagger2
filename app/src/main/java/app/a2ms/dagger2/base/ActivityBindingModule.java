a25d6d34a058358927652732476aa231b57a561d
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-01 16:02:44 +0100

    Add ActivityBindingModule abstract class Create ACTIVITY SCOPE
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

---
+++ app/src/main/java/app/a2ms/dagger2/base/ActivityBindingModule.java
@@ -0,0 +1,21 @@
+package app.a2ms.dagger2.base;
+
+import android.app.Activity;
+
+import app.a2ms.dagger2.home.MainActivity;
+import app.a2ms.dagger2.home.MainActivityComponent;
+import dagger.Binds;
+import dagger.Module;
+import dagger.android.ActivityKey;
+import dagger.android.AndroidInjector;
+import dagger.multibindings.IntoMap;
+
+@Module(subcomponents = {
+        MainActivityComponent.class,
+})
+public abstract class ActivityBindingModule {
+    @Binds
+    @IntoMap
+    @ActivityKey(MainActivity.class)
+    abstract AndroidInjector.Factory<? extends Activity> provideMainActivityInjector(MainActivityComponent.Builder builder);
+}

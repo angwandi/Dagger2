5fa1fcbf53803f0e89c1ae317bb1ad66a75f213c
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-02 16:52:52 +0100

    Lesson 1 FINISHED
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

--- app/src/main/java/app/a2ms/dagger2/home/MainActivityComponent.java
+++ app/src/main/java/app/a2ms/dagger2/home/MainActivityComponent.java
@@ -11,6 +11,10 @@ import dagger.android.AndroidInjector;
 public interface MainActivityComponent extends AndroidInjector<MainActivity> {
     @Subcomponent.Builder
     abstract class Builder extends AndroidInjector.Builder<MainActivity> {
+        //Prevents Injecting MainActivity in other classes
+        @Override
+        public void seedInstance(MainActivity instance) {
 
+        }
     }
 }
--- app/src/main/java/app/a2ms/dagger2/trending/TrendingReposController.java
+++ app/src/main/java/app/a2ms/dagger2/trending/TrendingReposController.java
@@ -5,9 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import javax.inject.Inject;
+
 import app.a2ms.dagger2.base.BaseController;
+import app.a2ms.dagger2.home.MainActivity;
 
 public class TrendingReposController extends BaseController {
+    @Inject
+    MainActivity mainActivity;
+    
     @NonNull
     @Override
     protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {

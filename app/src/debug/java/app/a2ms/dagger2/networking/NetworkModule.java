c1eaeec683e3b6f0fb241582a4b63dee2eb63fc0
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-03 22:37:01 +0100

    Fix baseUrl
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

--- app/src/debug/java/app/a2ms/dagger2/networking/NetworkModule.java
+++ app/src/debug/java/app/a2ms/dagger2/networking/NetworkModule.java
@@ -20,6 +20,6 @@ public abstract class NetworkModule {
     @Provides
     @Named("base_url")
     static String provideBaseUrl() {
-        return "https://api.ithub.com/";
+        return "https://api.github.com/";
     }
 }

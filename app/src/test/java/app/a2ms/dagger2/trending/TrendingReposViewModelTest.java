68cbb12b928def6383bb7503c431600199005f65
Innocent <31923567+angwandi@users.noreply.github.com>
2018-10-04 19:05:09 +0100

    Add TrendingReposViewModelsTest
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

---
+++ app/src/test/java/app/a2ms/dagger2/trending/TrendingReposViewModelTest.java
@@ -0,0 +1,47 @@
+package app.a2ms.dagger2.trending;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import app.a2ms.dagger2.R;
+import app.a2ms.dagger2.data.TrendingReposResponse;
+import io.reactivex.observers.TestObserver;
+
+public class TrendingReposViewModelTest {
+
+    private TrendingReposViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        viewModel = new TrendingReposViewModel();
+    }
+
+    @Test
+    public void loading() throws Exception {
+        TestObserver<Boolean> loadingObserver = viewModel.loading().test();
+        viewModel.loadingUpdated().accept(true);
+        viewModel.loadingUpdated().accept(false);
+
+        loadingObserver.assertValues(true, false);
+    }
+
+    @Test
+    public void repos() throws Exception {
+        TrendingReposResponse response = TestUtils.loadJson("mock/get_trending_repos.json", TrendingReposResponse.class);
+        viewModel.reposUpdated().accept(response.repos());
+
+        viewModel.repos().test().assertValue(response.repos());
+    }
+
+    @Test
+    public void error() throws Exception {
+        TestObserver<Integer> errorObserver = viewModel.error().test();
+        viewModel.onError().accept(new IOException());
+        viewModel.reposUpdated().accept(Collections.emptyList());
+
+        errorObserver.assertValues(R.string.api_error_repos, -1);
+    }
+}
